0bfe91bb86405d3e9cb51d9df3678d314141d778
yakagika <kaya3728+github@gmail.com>
2018-12-07 13:50:14 +0900

    to be lazy

--- src/CSVParserT.hs
+++ src/CSVParserT.hs
@@ -1,29 +1,42 @@
---file csvParser.hs
+-- file csvParserT.hs
 
-module CSVParserT  (parseCSVT,readCSVTWin,readCSVT) where
+{-# LANGUAGE BangPatterns #-}
 
-import qualified    Data.Text           as T
-import qualified    Data.Text.IO        as TO
+module CSVParserT  (parseCSVT,readCSVTWin,readCSVT,writeCSVT) where
+
+import qualified    Data.Text                   as T
+import              Data.Text                   (Text)
+import qualified    Data.Text.Lazy              as TL    
+import qualified    Data.Text.IO                as TO
+import qualified    Data.Text.Lazy.IO           as TLO
+import qualified    Data.List                   as L
 import              Text.Parsec
 import              Text.Parsec.Text
 import              Data.Maybe
 import              System.IO
+import              Control.DeepSeq
+import              Data.Either
+import Control.Parallel.Strategies hiding (parMap)
+import Control.Parallel
 
-csvFile = do x <- sepBy line eol
-             eof
-             return x
+parMap :: (a -> b) -> [a] -> Eval [b]
+parMap f [] = return []
+parMap f !(a:as)  = do
+    b  <- rpar (f a)
+    bs <- parMap f as
+    return (b:bs) 
 
+
+csvFile = sepBy line eol 
 line = sepBy cell (char ',') 
-
-cell = (quotedCell <|> many (noneOf ",\n\r")) >>= return . T.pack
+cell = (quotedCell <|> many (noneOf ",\n\r")) >>= (\res -> return $! T.pack res)
   
-quotedCell = 
-    do char  '"'
-       content <- many quotedChar
-       char '"' <?> "quote at end of cell"
-       return content
+quotedCell =  char  '"'
+           >> many quotedChar >>= \content 
+           -> (char '"' <?> "quote at end of cell")
+           >> return content
 
-quotedChar =
+quotedChar = 
         noneOf "\""
     <|> try (string  "\"\"" >> return '"')
 
@@ -33,7 +46,7 @@ eol =   try (string "\n\r")
     <|> string "\r"
     <?> "end of line"
 
-parseCSVT :: T.Text -> Either ParseError [[T.Text]]
+parseCSVT :: T.Text -> Either ParseError [[Text]]
 parseCSVT input = parse csvFile "Can not Parse Strings" input
 
 
@@ -41,26 +54,35 @@ parseCSVT input = parse csvFile "Can not Parse Strings" input
 -- encoding for Japanese on Windows
 cpWin = "cp932" 
 type Encode = String 
-type Path = String
 
-readCSVTWin :: Path -> IO [[T.Text]]
-readCSVTWin path    = openFile path ReadMode
-                    >>= \h -> mkTextEncoding cpWin 
+readCSVTWin :: FilePath -> IO [[Text]]
+readCSVTWin path    =   openFile path ReadMode  >>= \h 
+                    ->  mkTextEncoding cpWin 
                     >>= hSetEncoding h
-                    >> TO.hGetContents h 
-                    >>= \cs ->  
-                    case parseCSVT cs of
-                        Left e  -> putStrLn "Error parsing input:"
-                                >> print e >> return []
-                    
-                        Right r ->  return r
-                     
+                    >>  TLO.hGetContents h      >>= \cs 
+                    ->  return $ concat . runEval $ parMap ((fromRight []) . parseCSVT . TL.toStrict) $ TL.lines cs 
+                    -- Errorも扱えるようにする
 
-readCSVT :: Path -> IO [[T.Text]]
+
+readCSVT :: FilePath -> IO [[Text]]
 readCSVT path   = openFile  path ReadMode   >>= \h 
-                ->  TO.hGetContents h      >>= \cs 
-                ->  case parseCSVT cs of
+                ->  TLO.hGetContents h      >>= \cs 
+                ->  return $ concat . runEval $ parMap ((fromRight []) . parseCSVT . TL.toStrict) $ TL.lines cs 
+
+{-
+                        
                         Left e  -> putStrLn "Error parsing input:"
-                                >> print e >> return []
-                        Right r ->  return r
+                                >> print e >> return [] 
+                        Right r ->  return r            
+-}
+
+writeCSVT :: FilePath -> [[T.Text]] -> IO ()
+writeCSVT path xs   =  openFile path WriteMode >>= \handle 
+                    -> mapM_ ((TO.hPutStrLn handle) . T.concat . (L.intersperse (T.pack ","))) xs
+                    >> hClose handle
+
+
+-- for test
+main = do 
+    readCSVTWin "H24.txt" >>=  mapM_ (mapM_ TO.putStrLn)
 
